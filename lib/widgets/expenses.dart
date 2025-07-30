103a1ce158b7f10365b427dee5f3ec6055be9a12
Balazs Szedlacsek <balazs.szedlacsek@alzura.com>
2025-07-30 21:52:20 +0200

    Add basic new Expense modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -17,9 +18,21 @@ class _ExpensesState extends State<Expenses> {
     Expense(title: 'Beer', amount: 12.50, date: DateTime.now(), category: Category.leisure),
   ];
 
+  _openAddExpeneOverlay() {
+    // ctx means context
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense(),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense tracker'),
+        actions: [
+          IconButton(onPressed: _openAddExpeneOverlay, icon: const Icon(Icons.add))
+        ],
+      ),
+      body: Column(
       children: [
         Text('The cart'), 
         Expanded(child: ExpensesList(expenses: _reqisteredExpenses))
